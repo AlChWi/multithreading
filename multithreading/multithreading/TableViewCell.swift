9e153459e64236c9b91b4c81540fa416bfeb0151
Oleksii Perov <oleksii.perov@nure.ua>
2019-07-19 03:47:30 +0300

    added pictures

--- multithreading/multithreading/TableViewCell.swift
+++ multithreading/multithreading/TableViewCell.swift
@@ -11,11 +11,33 @@ import UIKit
 class TableViewCell: UITableViewCell {
 
     @IBOutlet weak var photoImageView: UIImageView!
+    @IBOutlet weak var photoActivityIndicator: UIActivityIndicatorView!
+    
+    private var path: String?
+    
+    override func prepareForReuse() {
+        photoImageView.image = nil
+    }
+    
+    func configure(path: String) {
+        self.path = path
+        photoActivityIndicator.startAnimating()
+        DispatchQueue.global(qos: .userInitiated).async { [weak self] in
+            if let url = URL(string: path), let data = try? Data(contentsOf: url), let image = UIImage(data: data), path == self?.path {
+                DispatchQueue.main.async {
+                    self?.photoImageView.image = image
+                    self?.photoActivityIndicator.stopAnimating()
+                }
+            }
+        }
+    }
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
     }
 
+    
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
 
--- multithreading/multithreading/ViewController.swift
+++ multithreading/multithreading/ViewController.swift
@@ -18,6 +18,8 @@ class ViewController: UIViewController {
             myTableView.register(nib, forCellReuseIdentifier: "MyNibCellId")
         }
     }
+    
+    
     var images: [String] = ["https://cdn.pixabay.com/photo/2015/05/28/14/53/ux-788002_1280.jpg",
     "https://cdn.pixabay.com/photo/2015/05/31/15/07/business-792113_1280.jpg",
     "https://image.shutterstock.com/image-photo/business-cards-mockup-on-color-600w-1034025634.jpg",
@@ -25,12 +27,12 @@ class ViewController: UIViewController {
     "https://image.shutterstock.com/z/stock-photo-two-colleagues-interior-designer-discussing-data-and-digital-tablet-and-computer-laptop-with-sample-370233089.jpg",
     "https://cdn.pixabay.com/photo/2014/05/02/21/49/home-office-336373_1280.jpg"]
     
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         // Do any additional setup after loading the view.
     }
-
-
 }
 
 extension ViewController: UITableViewDelegate, UITableViewDataSource {
@@ -39,9 +41,12 @@ extension ViewController: UITableViewDelegate, UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = myTableView.dequeueReusableCell(withIdentifier: "MyNibCellId", for: indexPath)
+        let cell = myTableView.dequeueReusableCell(withIdentifier: "MyNibCellId", for: indexPath) as! TableViewCell
+        cell.configure(path: images[indexPath.row])
         return cell
     }
     
-    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 300
+    }
 }
